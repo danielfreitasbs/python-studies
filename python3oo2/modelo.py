331108f70e95e2f40541b59a3ddd2e6fae7bb6de
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-10-27 10:44:47 -0300

    modulo4 - herança pt2

--- python3oo2/modelo.py
+++ python3oo2/modelo.py
@@ -41,13 +41,18 @@ class Serie(Programa):
         return f'Nome: {self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} likes'
 
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
 
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
     def tamanho(self):
-        return len(self.programas)
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
