de32025fd8f2d4f1b01713c518f376c941aedd35
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-10-27 10:16:16 -0300

    modulo3 - polimorfismo

--- python3oo2/modelo.py
+++ python3oo2/modelo.py
@@ -19,17 +19,26 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def imprime(self):
+        print(f'Nome: {self.nome} - {self.ano} - {self.likes} likes')
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f'Nome: {self.nome} - {self.ano} - {self.duracao} min - {self.likes} likes')
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def imprime(self):
+        print(f'Nome: {self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} likes')
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -41,5 +50,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
