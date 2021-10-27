2a0db2bd9925d04a01ef0c384cd341a158175b19
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-10-27 12:12:35 -0300

    modulo5 - duck typing

--- python3oo2/modelo.py
+++ python3oo2/modelo.py
@@ -46,12 +46,14 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
 
 
