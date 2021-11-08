71a980435f5cf02eb15aa572bd078395f2a778e2
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-11-08 17:19:29 -0300

    regex and special methods

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,17 +1,28 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-print(url)
+# url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
+url = ""
+
+# Sanitização da URL
+url = url.replace(" ", "")
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+# Separa base e os paremetros
 indice_interrogacao = url.find('?')
-
 url_base = url[:indice_interrogacao]
-print(url_base)
-
 url_parametros = url[(indice_interrogacao + 1):]
 print(url_parametros)
 
+# busca o valor de um parametro
 parametro_busca = 'moedaOrigem'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find(('&', indice_valor))
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 
 print(valor)
